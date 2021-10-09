66d3a949d4051394c423fb9aa82549ae0f7b6b01
sayidimrannazari <Sayidi_n@yahoo.com>
2021-10-09 11:04:34 -0400

    new staff updated imri

--- src/day48_static/MoreChaining.java
+++ src/day48_static/MoreChaining.java
@@ -16,4 +16,14 @@ public class MoreChaining {
         System.out.println("String one");
 //        this(5); any this() call needs to be the first line always
     }
+//    public MoreChaining(double d){
+//        this(10);
+//        this("java");
+   // }
+    // we can only call one constructor
+
+//    public MoreChaining(char c){
+//        this('d');
+//    }
+    // it's not possible to call yourself
 }
